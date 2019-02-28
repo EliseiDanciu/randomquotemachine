ed90b19c2e29c90c64eb5c61e4c1f79f87d6b11e
Elisei Danciu <elisei.danciu@senseisrl.it>
2019-02-28 17:59:46 +0100

    Initial commit

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import Axios from "axios";
+
+class QuoteBox extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			quotes: [],
+			isLoaded: false,
+			randomQuote: {}
+		};
+	}
+
+	componentDidMount() {
+		Axios.get(
+			"https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+		).then(res => {
+			this.setState({ quotes: res.data.quotes, isLoaded: true });
+			this.newRandomQuote();
+		});
+	}
+
+	newRandomQuote = e => {
+		const { quotes } = this.state;
+		this.setState({
+			randomQuote: quotes[Math.floor(Math.random() * quotes.length)]
+		});
+	};
+
+	render() {
+		const { quote, author } = this.state.randomQuote;
+		const { isLoaded } = this.state;
+		if (isLoaded) {
+			return (
+				<div
+					className="card card-body bg-light"
+					style={{ marginTop: "20%", height: "auto" }}
+				>
+					<div id="quote-box" className>
+						{}
+						<h2 id="text" className="text-center">
+							{quote}
+						</h2>
+						<p id="author" className="text-right">
+							- {author}
+						</p>
+
+						<button
+							id="new-quote"
+							onClick={this.newRandomQuote}
+							className="btn btn-primary float-right"
+						>
+							New Quote
+						</button>
+						<div>
+							<a
+								id="tweet-quote"
+								href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${encodeURIComponent(
+									'"' + quote + '" - ' + author
+								)}`}
+							>
+								<i style={{ size: 100 }} class="fab fa-twitter fa-lg" />
+							</a>
+						</div>
+					</div>
+				</div>
+			);
+		}
+		return null;
+	}
+}
+
+export default QuoteBox;
