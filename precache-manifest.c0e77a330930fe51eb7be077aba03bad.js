54465a6bc52e7865201646474d11a95b3817b9ef
Elisei Danciu <elisei.danciu@senseisrl.it>
2019-02-28 18:02:44 +0100

    Updates

---
+++ precache-manifest.c0e77a330930fe51eb7be077aba03bad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "287a46dda652264c08d3",
+    "url": "/randomquotemachine/static/css/main.8078cd28.chunk.css"
+  },
+  {
+    "revision": "287a46dda652264c08d3",
+    "url": "/randomquotemachine/static/js/main.287a46dd.chunk.js"
+  },
+  {
+    "revision": "3864e00a3abe55a57016",
+    "url": "/randomquotemachine/static/js/runtime~main.3864e00a.js"
+  },
+  {
+    "revision": "8493ceb3c34336a5ecde",
+    "url": "/randomquotemachine/static/js/2.8493ceb3.chunk.js"
+  },
+  {
+    "revision": "89e7fd8658caf6001d902fdd6384ecf2",
+    "url": "/randomquotemachine/index.html"
+  }
+];
